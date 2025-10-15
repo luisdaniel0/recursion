b22833142c04f06404abb7fec6a069052aca322c
Luis Sanchez <luisanchezdh@gmail.com>
2025-10-14 20:39:58 -0400

    completed fibonacci function using recursion

--- recursion.js
+++ recursion.js
@@ -3,9 +3,44 @@
 // Now write another function fibsRec which solves the same problem recursively.
 // Test both versions of your functions by passing in various lengths as arguments.
 
-function fibs(n) {
-  let arr = [0, 1];
+// function fibs(n) {
+//   //this is what we're starting out with because fibonacci sequence starts with 0,1 and then the sum of the prev 2 numbers
+//   let arr = [0, 1];
 
+//   //edge cases
+//   if (n === 0) {
+//     return [];
+//   }
+//   if (n === 1) {
+//     return [0];
+//   }
+//   if (n === 2) {
+//     return [0, 1];
+//   }
+
+//   for (i = 2; i < n; i++) {
+//     let fibNumers = arr[arr.length - 1] + arr[arr.length - 2];
+//     arr.push(fibNumers);
+//   }
+//   return arr;
+// }
+
+//2,3,4,5,6,7
+
+//2,3,4
+
+// [0,1,1,2,3,5,8,13]
+// fib sequence starts at 0,1 and then every number after that is the sum of the previous 2 numbers
+
+// an array containing 0,1
+// loop over the array
+// add the elements in the array as your looping them?
+// push the sums of each iteration into the array
+
+//RECURSIVE METHOD
+
+function fibsRec(n) {
+  //base cases
   if (n === 0) {
     return [];
   }
@@ -16,25 +51,14 @@ function fibs(n) {
     return [0, 1];
   }
 
-  for (i = 2; i < n; i++) {
-    let fibNumers = arr[arr.length - 1] + arr[arr.length - 2];
-    arr.push(fibNumers);
-  }
+  //recursive cases
+  let arr = fibsRec(n - 1);
+  let sumArray = arr[arr.length - 1] + arr[arr.length - 2];
+  arr.push(sumArray);
   return arr;
 }
 
-//2,3,4,5,6,7
-
-//2,3,4
-
-console.log(fibs(8));
-console.log(fibs(0));
-console.log(fibs(1));
-console.log(fibs(2));
-// [0,1,1,2,3,5,8,13]
-// fib sequence starts at 0,1 and then every number after that is the sum of the previous 2 numbers
-
-// an array containing 0,1
-// loop over the array
-// add the elements in the array as your looping them?
-// push the sums of each iteration into the array
+console.log(fibsRec(8));
+console.log(fibsRec(0));
+console.log(fibsRec(1));
+console.log(fibsRec(2));
